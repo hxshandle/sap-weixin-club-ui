be6bb4354ac9b9b27221560df73a71bcf8f48695
Huang <handle.huang@sap.com>
2017-05-31 16:15:43 +0800

    ab

---
+++ origin/club-info-activities/data.js
@@ -0,0 +1,33 @@
+﻿var fill = {
+  "data": [{
+    "etype": "0",
+    "id": "15",
+    "usercnt": "1",
+    "lastupdate": "2017-05-04 16:01:35",
+    "location": "",
+    "owner": "1",
+    "nickname": "Peng,Yang",
+    "title": "上线前第一次测试上线前第一次测试:",
+    "clubid": "6",
+    "clubname": "微信测试俱乐部微信测试俱乐部微信测试俱乐部微信测试俱乐部微信测试俱乐部",
+    "filename": "http://sallyaroundthebay.com/wp-content/uploads/2013/06/duck-Ernst-Vikne-300x300.jpg",
+    "fflag": "0",
+    "cnt": 0
+  }],
+  "headers": ["etype", "id", "usercnt", "lastupdate", "location", "owner", "nickname", "title", "clubid", "clubname", "filename", "fflag"],
+  "udata": {
+    "_all": [],
+    "_opt": {
+      "skipData": null,
+      "dataSpec": null,
+      "inputFields": null
+    }
+  },
+  "option": {
+    "skipData": null,
+    "dataSpec": null,
+    "inputFields": null
+  },
+  "uo": "0",
+  "id": "cont"
+};
