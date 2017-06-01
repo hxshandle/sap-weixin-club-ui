ac1d81a20f66408b86bc5c1378006442be1007d5
Huang <handle.huang@sap.com>
2017-06-01 17:17:53 +0800

    ab

---
+++ origin/club-create/data.js
@@ -0,0 +1,110 @@
+var fill = {
+  "data": [{
+    "id": "5",
+    "owner": "1",
+    "flag": "1",
+    "companyid": "1",
+    "createdate": "",
+    "lastupdate": "",
+    "title": "乒乓球俱乐部",
+    "logoid": "8",
+    "summary": "测试",
+    "usercnt": "3",
+    "filename": "http://cos.bizof.me/337DA830FA6EAA35EF3CFAA082BD6972.png",
+    "cnt": 0
+  }, {
+    "id": "6",
+    "owner": "4",
+    "flag": "1",
+    "companyid": "1",
+    "createdate": "",
+    "lastupdate": "",
+    "title": "微信测试俱乐部",
+    "logoid": "10",
+    "summary": "测试俱乐部本身的功能，有问题请留言\n...",
+    "usercnt": "2",
+    "filename": "http://cos.bizof.me/C310B4BC2A12C0C3A18575A77DBE0F8C.png",
+    "cnt": 0
+  }, {
+    "id": "7",
+    "owner": "5",
+    "flag": "1",
+    "companyid": "1",
+    "createdate": "",
+    "lastupdate": "",
+    "title": "Dance Club",
+    "logoid": "13",
+    "summary": "爱跳舞的程序媛们",
+    "usercnt": "13",
+    "filename": "http://cos.bizof.me/C67AC51CE6106CA8C7B1D0F05F5AF2F0.jpg",
+    "cnt": 0
+  }, {
+    "id": "11",
+    "owner": "6",
+    "flag": "1",
+    "companyid": "1",
+    "createdate": "",
+    "lastupdate": "",
+    "title": "SAP Labs China Toastmasters Club",
+    "logoid": "17",
+    "summary": "Toastmasters Club,...",
+    "usercnt": "3",
+    "filename": "http://cos.bizof.me/5887555927AE759F100A37EDA9BB7BE6.jpg",
+    "cnt": 0
+  }, {
+    "id": "8",
+    "owner": "10",
+    "flag": "1",
+    "companyid": "1",
+    "createdate": "",
+    "lastupdate": "",
+    "title": "Labs China Runner Club",
+    "logoid": "0",
+    "summary": "",
+    "usercnt": "2",
+    "filename": "./3rd/images/empty.gif",
+    "cnt": 0
+  }, {
+    "id": "9",
+    "owner": "20",
+    "flag": "1",
+    "companyid": "1",
+    "createdate": "",
+    "lastupdate": "",
+    "title": "骑行俱乐部",
+    "logoid": "15",
+    "summary": "",
+    "usercnt": "2",
+    "filename": "http://cos.bizof.me/7239FBA73D89474A09FDFA3982C48091.jpg",
+    "cnt": 0
+  }, {
+    "id": "10",
+    "owner": "22",
+    "flag": "1",
+    "companyid": "1",
+    "createdate": "",
+    "lastupdate": "",
+    "title": "羽毛球俱乐部",
+    "logoid": "0",
+    "summary": "",
+    "usercnt": "1",
+    "filename": "./3rd/images/empty.gif",
+    "cnt": 0
+  }],
+  "headers": ["id", "owner", "flag", "companyid", "createdate", "lastupdate", "title", "logoid", "summary", "usercnt", "filename"],
+  "udata": {
+    "_all": [],
+    "_opt": {
+      "skipData": null,
+      "dataSpec": null,
+      "inputFields": null
+    }
+  },
+  "option": {
+    "skipData": null,
+    "dataSpec": null,
+    "inputFields": null
+  },
+  "uo": "0",
+  "id": "cont"
+};
